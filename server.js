8c16ea815e113203786d04733d1712cbabbce8f1
ulrich2k25 <ulrichpouke150@gmail.com>
2025-03-22 00:42:26 +0100

    Fix doublon déclaration url

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
-const { URL } = require("url");
+
 const cors = require("cors");
 const multer = require("multer");
 const AWS = require("aws-sdk");
