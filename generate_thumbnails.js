e8152cf2b193d22b9397856baf47df487ac77fc1
ulrich2k25 <ulrichpouke150@gmail.com>
2025-04-07 00:40:32 +0200

    ✅ Remplacement par version ESModule de generate_thumbnails1

--- generate_thumbnails.js
+++ generate_thumbnails.js
@@ -17,12 +17,30 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION
 });
 
-const videosFolder = path.join(__dirname, 'videos');
-const thumbnailsFolder = path.join(__dirname, 'thumbnails');
+//const videosFolder = path.join(__dirname, 'videos');
+//const thumbnailsFolder = path.join(__dirname, 'thumbnails');
+const videoPath = "C:/Users/pouke/Desktop/New_Projekt/video1.mp4";
+const thumbnailPath = "C:/Users/pouke/Desktop/New_Projekt/thumbnail1.jpg";
+
 
 if (!fs.existsSync(thumbnailsFolder)) {
   fs.mkdirSync(thumbnailsFolder);
 }
+ffmpeg(videoPath)
+  .screenshots({
+    timestamps: ['5'],
+    filename: path.basename(thumbnailPath),
+    folder: path.dirname(thumbnailPath),
+    size: '320x240'
+  })
+  .on('end', () => {
+    console.log('Miniature générée avec succès à :', thumbnailPath);
+  })
+  .on('error', (err) => {
+    console.error('Erreur ffmpeg :', err);
+  });
+
+
 
 fs.readdir(videosFolder, (err, files) => {
   if (err) return console.error('Erreur lecture dossier vidéos:', err);
